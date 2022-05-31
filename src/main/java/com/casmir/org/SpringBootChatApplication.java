26a18efefbe664b58a24f212e5f29a46b84ebcb7
ugwuchukwuogbonnaya <stcasmir4excel@gmail.com>
2022-05-31 17:10:09 +0100

    first commit

---
+++ src/main/java/com/casmir/org/SpringBootChatApplication.java
@@ -0,0 +1,13 @@
+package com.casmir.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChatApplication.class, args);
+	}
+
+}
